a102a0dfa828d2345f3eac9cc6d1b362e3944ec0
Ryan <106076362+bry1ni@users.noreply.github.com>
2023-11-05 01:51:45 +0100

    Add files via upload

---
+++ main/java/com/example/bastion23/CameraActivity.java
@@ -0,0 +1,56 @@
+package com.example.bastion23;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CameraActivity extends AppCompatActivity {
+
+    ImageView image;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        image = findViewById(R.id.img_capture);
+        button = findViewById(R.id.btn_capture);
+
+        if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, 100);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 100);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(bitmap);
+
+        }
+    }
+}
